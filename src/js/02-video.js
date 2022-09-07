c59d9197296fef7964dea64b9066d477586e70aa
Arthur Korostelev <korostelev.art.ua@gmail.com>
2022-09-07 16:07:15 +0300

    created 2-3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimedUpdate, 1000));
+function onTimedUpdate(e) {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+};
+
+player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time'))).then(function (seconds) {
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+            
+    }
+})
